cc695f1c513bc108aafbe80077daf412b88f7eea
c0nniewang <connie.wang303@gmail.com>
2018-01-19 10:24:32 -0500

    working on new marker

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -32,7 +32,7 @@ class MapContainer extends React.PureComponent {
   render() {
     return (
       <Map
-        markers={this.state.markers}
+        existingMarkers={this.state.markers}
         currentLocation={{lat: 43.587332, lng: -110.829230}}
         isMarkerShown={this.state.isMarkerShown}
         onMarkerClick={this.handleMarkerClick}
